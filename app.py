f3c6edc57fe03985b6b07885f54e45634a18ec34
Chi Le <letuechi.2006@gmail.com>
2025-07-10 12:12:58 +0200

    few tweaks and cleaning

--- app.py
+++ app.py
@@ -13,10 +13,17 @@ def home():
         finder = request.form.get("finder")
 
         # Validate the input against allowed templates (the exact names and similar names)
-        allowed_templates = ["about", "client", "collab", "personal", "street", "olympics",
-                             "highlander", "illustration", "recit", "superface"]  # Add valid template names
-        if finder.lower() in allowed_templates:
+        personal_websites = ["afvs", "essays", "fysemr", "illustration", "portal", "street", "superface"] 
+        collab_websites = ["highlander", "recit", "olympics"]
+        flat = ["about", "client", "collab", "personal"]
+        # Add valid template names
+        if finder.lower() in flat:
             return render_template(f"{finder}.html")
+        elif finder.lower() in personal_websites:
+            return render_template(f"personal websites/{finder}.html")
+        elif finder.lower() in collab_websites:
+            return render_template(f"collab websites/{finder}.html")
+        
         elif finder.lower() in ["chi", "pirenily", "me", "chi le", "emily", "iron pig", "chi bell", "myself", "i", "artist"]:
             return render_template("about.html")
         elif finder.lower() in ["commission", "client work", "commissioned work", "commissions", "client"]:
@@ -25,16 +32,18 @@ def home():
             return render_template("collab.html")
         elif finder.lower() in ["personal work", "self", "person", "mine", "free", "journey"]:
             return render_template("personal.html")
+        
         else:
-            allowed_templates = ["about", "client", "collab", "personal",
-                                 "illustration", "portal", "recit", "street"]  # Add valid template names
-            finder = random.choice(allowed_templates)
+            finder = random.choice(personal_websites + collab_websites + flat)
             # Abort with a 404 error if the template is not allowed
-            return render_template(f"{finder}.html")
+            if finder in personal_websites:
+                return render_template(f"personal websites/{finder}.html")
+            elif finder in collab_websites:
+                return render_template(f"collab websites/{finder}.html")
+            else:
+                return render_template(f"{finder}.html")
 
 # Generate template for each html page
-
-
 @app.route("/about")
 def about():
     return render_template("about.html")
@@ -57,51 +66,45 @@ def collab():
 
 @app.route("/illustration")
 def illustration():
-    return render_template("illustration.html")
+    return render_template("personal websites/illustration.html")
 
 
 @app.route("/portal")
 def portal():
-    return render_template("portal.html")
-
-
-@app.route("/recit")
-def recit():
-    return render_template("recit.html")
-
+    return render_template("personal websites/portal.html")
 
 @app.route("/street")
 def street():
-    return render_template("street.html")
-
-
-@app.route("/highlander")
-def highlander():
-    return render_template("highlander.html")
-
+    return render_template("personal websites/street.html")
 
 @app.route("/superface")
 def superface():
-    return render_template("superface.html")
-
-
-@app.route("/olympics")
-def olympics():
-    return render_template("olympics.html")
+    return render_template("personal websites/superface.html")
 
 
 @app.route("/afvs")
 def afvs():
-    return render_template("afvs.html")
+    return render_template("personal websites/afvs.html")
 
 @app.route("/fysemr")
 def fysemr():
-    return render_template("fysemr.html")
+    return render_template("personal websites/fysemr.html")
 
 @app.route("/essays")
 def essays():
-    return render_template("essays.html")
+    return render_template("personal websites/essays.html")
 
+@app.route("/highlander")
+def highlander():
+    return render_template("collab websites/highlander.html")
+
+@app.route("/recit")
+def recit():
+    return render_template("collab websites/recit.html")
+
+@app.route("/olympics")
+def olympics():
+    return render_template("collab websites/olympics.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
